0287d06f3bf2187a72fdf2585203d52dcac90e6a
Abito21 <abidjuliant@gmail.com>
2022-12-07 07:57:33 +0700

    process-object

--- nodejs-basic/app.js
+++ nodejs-basic/app.js
@@ -3,14 +3,15 @@
 // console.log(cpuInformation);
 
 /// Untuk menjalankan process.argv
-// const firstName = process.argv[2];
-// const lastName = process.argv[3]; 
-// console.log(`Hello ${firstName} ${lastName}`);
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+console.log(`Hello ${firstName} ${lastName}`);
 
 /// Untuk menjalankan process.env (untuk menyimpan manual data ke dalam process env)
 /// dengan membuat properti NODE_ENV
-// const server = new Server({
-//     host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
-// });
+const server = new Server({
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
+});
+
 /// Menjalankannya menggunakan CMD
 // SET NODE_ENV=production && node app.js
