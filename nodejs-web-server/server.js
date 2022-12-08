29c7d70bdfca32a0442120630f0b529c72fe3b53
Abito21 <abidjuliant@gmail.com>
2022-12-08 18:17:13 +0700

    method/verb request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -9,11 +9,26 @@ const http = require('http');
  
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
- 
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
-};
  
+    const { method } = request;
+ 
+    if(method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+ 
+    if(method === 'POST') {
+        response.end('<h1>Hai!</h1>');
+    }
+ 
+    if(method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+ 
+    if(method === 'DELETE') {
+        response.end('<h1>Salam!</h1>');
+    }
+};
  
 const server = http.createServer(requestListener);
  
@@ -27,4 +42,11 @@ server.listen(port, host, () => {
 // Keterangan :
 // Selamat! Anda berhasil membuat HTTP Server pertama menggunakan Node.js.
 // Anda bisa coba melakukan request pada server tersebut melalui cURL seperti ini:
-// curl -X GET http://localhost:5000/
+// curl -X POST http://localhost:5000
+// // output: <h1> Halo HTTP Server!</h1>
+// curl -X PUT http://localhost:5000
+// // output: <h1> Halo HTTP Server!</h1>
+// curl -X DELETE http://localhost:5000
+// // output: <h1> Halo HTTP Server!</h1>
+// curl -X GET http://localhost:5000
+// output: <h1> Halo HTTP Server!</h1>
