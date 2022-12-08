ed115586b935469aad02feff6ac9e596df5ef273
Abito21 <abidjuliant@gmail.com>
2022-12-08 18:38:19 +0700

    Body Request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -13,20 +13,24 @@ const requestListener = (request, response) => {
  
     const { method } = request;
  
+    // GET
     if(method === 'GET') {
         response.end('<h1>Hello!</h1>');
     }
  
+    // POST
     if(method === 'POST') {
-        response.end('<h1>Hai!</h1>');
-    }
- 
-    if(method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
- 
-    if(method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
+        let body = [];
+    
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+       
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+          });
     }
 };
  
